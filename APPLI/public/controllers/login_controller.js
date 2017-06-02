6cc22802ccc00c57096a78bdc4d7f23bbb30a15d
root <legal.quentin>
2017-06-02 09:35:37 +0200

    add working version of user connection

--- APPLI/public/controllers/login_controller.js
+++ APPLI/public/controllers/login_controller.js
@@ -3,12 +3,31 @@
 var mod = angular.module("login", ["ngRoute"]);
 
 mod.config( ["$routeProvider", function($routeProvider) {
-    $routeProvider.when('/login', {
-      templateUrl: 'pages/login.html',
-      controller: 'loginCtrl'
-    });
+  $routeProvider.when('/login', {
+    templateUrl: 'pages/login.html',
+    controller: 'loginCtrl'
+  });
 }]);
 
-mod.controller('loginCtrl', function() {
-
-})
+mod.controller('loginCtrl', function($scope, $http, $location) {
+  $scope.login = function() {
+    $http({
+      method: 'POST',
+      url: '/connection',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      transformRequest: function(obj) {
+          var str = [];
+          for(var p in obj)
+          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+          return str.join("&");
+      },
+      data: {email: $scope.email, password: $scope.password}
+  }).then(function (response) {
+      if(response.data == 200)
+        $location.path("/profile");
+      else {
+        alert("WRONG CREDENTIALS !")
+      }
+  });
+  }
+});
--- APPLI/public/controllers/register_controller.js
+++ APPLI/public/controllers/register_controller.js
@@ -3,13 +3,32 @@
 var mod = angular.module("register", ["ngRoute"]);
 
 mod.config( ["$routeProvider", function($routeProvider) {
-    
+
     $routeProvider.when('/register', {
       templateUrl: 'pages/register.html',
       controller: 'registerCtrl'
     });
 }]);
 
-mod.controller('register', function() {
-
+mod.controller('registerCtrl', function($scope, $http, $location) {
+  $scope.register = function() {
+    $http({
+      method: 'POST',
+      url: '/register',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      transformRequest: function(obj) {
+          var str = [];
+          for(var p in obj)
+          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+          return str.join("&");
+      },
+      data: {name: $scope.name, email: $scope.email, password: $scope.password}
+  }).then(function (response) {
+      if(response.data == 200)
+        $location.path("/profile");
+      else {
+        alert(response.data);
+      }
+  });
+  }
 });
