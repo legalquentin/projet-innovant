c2d2ea098b91e0e8ab4ac7d5f9e92c296558d4a5
Quentin Le Gal <legal_q@etna-alternance.net>
2017-06-26 09:40:09 +0200

    Working version 0.1

--- APPLI/src/main/java/app/connection/Serializer.java
+++ APPLI/src/main/java/app/connection/Serializer.java
@@ -3,6 +3,8 @@ package app;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import java.security.MessageDigest;
+import java.security.Security;
 
 // this class is used to serialize incoming data from http requests
 // and create a ResponseBody for the client
@@ -23,5 +25,24 @@ public class Serializer {
       ex.printStackTrace();
       return CreateResponseBody(500, "Internal Server Error");
     }
+  }
+
+  String encodePwd(String message) {
+    String generatedPassword = null;
+    try {
+      MessageDigest md = MessageDigest.getInstance("MD5");
+      md.update(message.getBytes());
+      byte[] bytes = md.digest();
+      StringBuilder sb = new StringBuilder();
+      for(int i=0; i< bytes.length ;i++) {
+        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+      }
+      generatedPassword = sb.toString();
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+    return generatedPassword;
   }
 }
